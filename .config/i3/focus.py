66d01437e968f842738bcda65fe81108cb00d1ad
Andrew Muncie <muncie88@hotmail.com>
2022-03-26 20:35:53 +0000

    added i3blocks.conf.secondary & focus.py

---
+++ .config/i3/focus.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import i3ipc
+import json
+
+i3 = i3ipc.Connection()
+
+
+def on_focus(i3, e):
+    current = i3.get_tree().find_focused()
+    current_ws_name = current.workspace().name
+    # print(type(i3.get_workspaces()))
+    ws_list = i3.get_workspaces()
+    for ws in ws_list:
+        if ws.name == current_ws_name:
+            print(ws.output)
+            if ws.output == 'DVI-I-2':
+                i3.command("mode default")
+            elif ws.output == 'DVI-I-3':
+                #i3.command('mode  ')
+                #i3.command('mode ')
+                #i3.command('mode  ')
+                i3.command('mode  ')
+            else:
+                i3.command('mode default')
+            break
+#        print(type(ws))
+
+
+i3.on('workspace::focus', on_focus)
+
+i3.main()
+#outputs = json.dumps(i3.get_outputs())
+# print(outputs)
